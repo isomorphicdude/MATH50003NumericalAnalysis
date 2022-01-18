2c0f60d85879932347d52c964c338a288b7faf44
isomorphicdude <iamthesenate@qq.com>
2022-01-18 22:51:23 +0000

    do not know what he has done

--- src/MATH50003NumericalAnalysis.jl
+++ src/MATH50003NumericalAnalysis.jl
@@ -8,6 +8,8 @@ using Weave
 kwds = (out_path = "notebooks/", jupyter_path = "$(homedir())/.julia/conda/3/bin/jupyter", nbconvert_options = "--allow-errors")
 
 notebook("src/Julia.jmd"; kwds...)
+notebook("src/Asymptotics.jmd"; kwds...)
+
 notebook("src/Numbers.jmd"; kwds...)
 notebook("src/Differentiation.jmd"; kwds...)
 
@@ -28,6 +30,7 @@ notebook("src/Integration.jmd"; kwds...)
 
 kwds = (out_path = "sheets/", jupyter_path = "$(homedir())/.julia/conda/3/bin/jupyter", nbconvert_options = "--allow-errors")
 notebook("src/week1.jmd"; kwds...)
+notebook("src/week2.jmd"; kwds...)
 
 ##
 # solutions 
